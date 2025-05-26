f35b09dda7aaa6bef4797a237d4335de4f26aaf5
ramadani <hubillahpunk@gmail.com>
2025-05-26 14:27:52 +0700

    persiapan edit data

--- src/views/EditCaffeView.vue
+++ src/views/EditCaffeView.vue
@@ -1,8 +1,49 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
 
+const router = useRouter();
+
+const name = ref('')
+const description = ref('')
+const time = ref('')
+
+const saveData = async () => {
+    const newCaffe = JSON.stringify({
+        name: name.value,
+        description: description.value,
+        time: Date.parse(time.value)/1000,
+    })
+
+    const response = await fetch('/api/caffes', {
+        method:'POST',
+        body: newCaffe,
+    })
+    const data = await response.json()
+
+    router.push('/')
+}
 </script>
-
 
 <template>
     <h1>Edit Caffe Baru</h1>
+
+     <form @submit.prevent="saveData">
+        <div>
+            <label>Nama</label>
+            <input type="text" v-model="name">
+        </div>
+        <div>
+            <label>Deskripsi</label>
+            <input type="text" v-model="description">
+        </div>
+        <div>
+            <label>Waktu</label>
+            <input type="datetime-local" v-model="time">
+        </div>
+
+        <div>
+            <input type="submit">
+        </div>
+    </form>
 </template>
