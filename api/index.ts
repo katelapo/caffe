9cfe454cb5455fffcbd4ab709484275f67abfdc4
ramadani <hubillahpunk@gmail.com>
2025-05-26 14:37:40 +0700

    update dan delete data

--- api/index.ts
+++ api/index.ts
@@ -7,17 +7,42 @@ type Bindings = {
 
 const app = new Hono<{ Bindings: Bindings }>()
 
-app.get('/api/events', async (c) => {
-  let { results } = await c.env.DB.prepare("SELECT * FROM events").all()
+app.get('/api/caffes', async (c) => {
+  let { results } = await c.env.DB.prepare("SELECT * FROM caffes").all()
   return c.json(results)
 })
 
-  app.post('/api/events', async (c) => {
-    const newId = crypto.randomUUID()
-    const input = await c.req.json<any>()
-    const query = `INSERT INTO events(id,name,place,time) values ("${newId}","${input.name}","${input.place}",${input.time})`
-    const newEvent = await c.env.DB.exec(query)
-    return c.json(newEvent)
+app.post('/api/caffes', async (c) => {
+  const newId = crypto.randomUUID()
+  const input = await c.req.json<any>()
+  const query = `INSERT INTO caffes(id,name,description,time) values ("${newId}","${input.name}","${input.description}",${input.time})`
+  const newCaffe = await c.env.DB.exec(query)
+  return c.json(newCaffe)
+})
+
+app.get('/api/caffes/:id', async (c) => {
+  const caffeId = c.req.param('id')
+  let { results } = await c.env.DB.prepare('SELECT * FROM caffes WHERE id = ?').bind(caffeId).all()
+  return c.json(results[0])
+})
+
+app.put('/api/caffes/:id', async (c) => {
+  const caffeId = c.req.param('id')
+
+  const input = await c.req.json<any>()
+  const query = `UPDATE caffes SET name = "${input.name}", description = "${input.description}", time = ${input.time} WHERE id = "${caffeId}"`
+  const caffe = await c.env.DB.exec(query)
+
+  return c.json(caffe)
+})
+
+app.delete('/api/caffes/:id', async (c) => {
+  const caffeId = c.req.param('id')
+
+  const query = `DELETE FROM caffes WHERE id = "${caffeId}"`
+  const caffe = await c.env.DB.exec(query)
+
+  return c.json(caffe)
 })
 
 export default app
