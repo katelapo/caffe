6f4cc33629312ff30d6e810b72324beba1ef2dc4
ramadani <hubillahpunk@gmail.com>
2025-05-26 14:35:47 +0700

    menggunakan hono untuk backend

--- api/index.ts
+++ api/index.ts
@@ -1,24 +1,23 @@
-interface Env {
-    ASSETS: Fetcher;
-    DB: D1Database;
+import { Hono } from 'hono'
+
+type Bindings = {
+  ASSETS: Fetcher
+  DB: D1Database
 }
 
-export default {
-    async fetch(request, env) {
-        const url = new URL(request.url);
+const app = new Hono<{ Bindings: Bindings }>()
 
-        if (url.pathname.startsWith("/api/caffes")){
-            if (request.method == 'GET') {
-        let { results } = await env.DB.prepare("SELECT * FROM caffes").all();
-        return Response.json(results);
-      } else if (request.method == 'POST') {
-        const newId = crypto.randomUUID()
-        const input = await request.json<any>()
-        const query = `INSERT INTO caffes(id,name,description,time) values ("${newId}","${input.name}","${input.description}",${input.time})`;
-        const newCaffe = await env.DB.exec(query);
-        return Response.json(newCaffe);
-      }
-        }
-        return env.ASSETS.fetch(request);
-    },
-}satisfies ExportedHandler<Env>;
+app.get('/api/events', async (c) => {
+  let { results } = await c.env.DB.prepare("SELECT * FROM events").all()
+  return c.json(results)
+})
+
+  app.post('/api/events', async (c) => {
+    const newId = crypto.randomUUID()
+    const input = await c.req.json<any>()
+    const query = `INSERT INTO events(id,name,place,time) values ("${newId}","${input.name}","${input.place}",${input.time})`
+    const newEvent = await c.env.DB.exec(query)
+    return c.json(newEvent)
+})
+
+export default app
